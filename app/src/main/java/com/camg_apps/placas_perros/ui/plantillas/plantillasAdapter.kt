59bbd03fc2bebb57ab29072b65d3b0ffca90e3c6
Christian Magaña <cmaganapc@corporativosade.com.mx>
2022-09-15 17:02:52 -0500

    translated

--- app/src/main/java/com/camg_apps/placas_perros/ui/plantillas/plantillasAdapter.kt
+++ app/src/main/java/com/camg_apps/placas_perros/ui/plantillas/plantillasAdapter.kt
@@ -1,5 +1,6 @@
 package com.camg_apps.placas_perros.ui.plantillas
 
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import com.camg_apps.placas_perros.R
 import com.camg_apps.placas_perros.data.Template
+import com.camg_apps.placas_perros.databinding.ActivityMainBinding.inflate
 import com.camg_apps.placas_perros.databinding.FragmentPlantillaIFEBinding
 import com.camg_apps.placas_perros.plantillas.plantillaIFEFragment
 import com.camg_apps.placas_perros.plantillas.plantillaINEFragment
@@ -20,15 +22,22 @@ class plantillasAdapter(
     private val navController: NavController
 ) : RecyclerView.Adapter<plantillasAdapter.ViewHolder>() {
 
+    var ctx: Context? = null;
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_plantillas, parent, false)
+        ctx = parent.context
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_plantillas, parent, false)
+
+
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.ivDesign.setImageResource(item.drawableTemplateFrontal!!)
+       // holder.ivDesign.setImageResource(item.drawableTemplateFrontal!!)
+        val cvDeltantera = LayoutInflater.from(ctx).inflate(item.layoutFrontal ,null) as View
+
+        holder.cvPlantilla.addView(cvDeltantera)
         holder.cvPlantilla.setOnClickListener {
             val bundle = bundleOf(plantillaIFEFragment.EXTRA_TEMPLATE to item)
             navController.navigate(R.id.action_nav_plantillas_to_plantillaIFEFragment, bundle)
@@ -38,7 +47,7 @@ class plantillasAdapter(
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val ivDesign: ImageView = view.findViewById(R.id.imageViewDesign)
+       // val ivDesign: ImageView = view.findViewById(R.id.imageViewDesign)
         val cvPlantilla: ConstraintLayout = view.findViewById(R.id.cvPlantilla)
 
     }
